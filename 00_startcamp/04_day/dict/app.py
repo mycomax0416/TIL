c7b7abd9bf8b3209d26c39fa13e7c5ff282790b1
mycomax0416 <mycomax0416@gmail.com>
2019-07-12 16:13:26 +0900

    2019-07-12 | First commit

--- 00_startcamp/04_day/dict/app.py
+++ 00_startcamp/04_day/dict/app.py
@@ -55,7 +55,7 @@
     
 
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -66,41 +66,49 @@ def lotto_check():
 @app.route('/lotto_result')
 def lotto_result():
     # 회차 번호를 받아온다.
-    num = requests.args.get('num')
+    num = request.args.get('num')
     # 동행복권에 요청을 보내 응답을 받는다.
-    res = requests.get(f'{num}')
+    res = requests.get(f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}')
     # json 형태로 바꿔준다. (우리가 크롬에서 보고있는 결과와 동일한 모습)
     lotto = res.json()
 
     # 당첨번호 6개만 가져오기
     winner = []
     for i in range(1, 7):
-        winner.append(lotto[f'drwtoNo{i}'])
+        winner.append(lotto[f'drwtNo{i}'])
 
     # 내 번호 리스트 만들기
     numbers = []
-    for num in requests.args.get('numbers').split()
+    for num in request.args.get('numbers').split():
         numbers.append(int(num))
 
     # 등수 가리기(몇개 맞았는지 교집합이 필요)
     # 내 번호 요소를 뽑아서 당첨번호 리스트에 있는지 확인.
     matched = 0
+    # 내 번호 리스트를 돌면서
     for num in numbers:
         if num in winner:
             matched += 1
-    if matched == 6:
-        result = '1등입니다!'
-    elif matched == 5:
-        if lotto['bnusNo'] in numbers:
-            result = '2등입니다!'
+    # 위에 코드를 교집합으로 코딩
+    # mathed = len(set(winner) & set(numbers))
+
+    if len(numbers) == 6:
+        if matched == 6:
+            result = '1등입니다!'
+        elif matched == 5:
+            # 보너스 번호가 내 로또번호 리스트에 존재하면,
+            if lotto['bnusNo'] in numbers:
+                result = '2등입니다!'
+            else:
+                result = '3등입니다!'
+        elif matched == 4:
+            result = '4등입니다!'
+        elif matched == 3:
+            result = '5등입니다!'
         else:
-            result = '3등입니다!'
-    elif matched == 4:
-        result = '4등입니다!'
-    elif matched == 5:
-        result = '5등입니다!'
+            result = '꽝입니다!'
     else:
-        result = '꽝입니다!'
+        result = '번호의 수가 6개가 아닙니다.'
 
     return render_template('lotto_result.html', winner=winner, numbers=numbers, result=result)
 
---
+++ 00_startcamp/telegram_bot/telegram_bot.md
@@ -0,0 +1,47 @@
+bot name : gakyung / gakyung_bot
+
+874909108:AAG5kS7pJDKvHPSvmxYPMUpkJm2J9MCnovE
+
+https://api.telegram.org/bot<token>/METHOD_NAME
+
+https://api.telegram.org/bot874909108:AAG5kS7pJDKvHPSvmxYPMUpkJm2J9MCnovE/getMe
+
+chat_it : 848364237
+
+
+
+sendmessage
+
+https://api.telegram.org/bot874909108:AAG5kS7pJDKvHPSvmxYPMUpkJm2J9MCnovE/sendMessage?chat_id=848364237&text=안녕하세요
+
+
+
+```Session Status                online
+Session Expires               7 hours, 59 minutes
+Version                       2.3.30
+Region                        United States (us)
+Web Interface                 http://127.0.0.1:4040
+Forwarding                    http://74aa3014.ngrok.io -> http://localhost:5000
+Forwarding                    https://74aa3014.ngrok.io -> http://localhost:5000
+
+Connections                   ttl     opn     rt1     rt5     p50     p90
+                              0       0       0.00    0.00    0.00    0.00
+```
+
+
+
+webhook 등록 주소
+
+https://api.telegram.org/bot874909108:AAG5kS7pJDKvHPSvmxYPMUpkJm2J9MCnovE/setWebhook?url=https://mycomax.pythonanywhere.com/874909108:AAG5kS7pJDKvHPSvmxYPMUpkJm2J9MCnovE
+
+
+
+### [mycomax.pythonanywhere.com](http://mycomax.pythonanywhere.com/)
+
+xqFzfGR1KCenpIPaCzib
+
+9Y1ihBA7Ka
+
+
+
+배포(deploy) - Pythonanywhere
